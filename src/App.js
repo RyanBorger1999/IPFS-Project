e437dec7598648f74a5a1c653190b20e00e6ced5
rlb8800 <rlb8800@g.rit.edu>
2022-05-08 18:23:46 -0400

    Change test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          IPFS Tutorial
+          IPFS Tutorial Change Test
         </p>
       </header>
     </div>
